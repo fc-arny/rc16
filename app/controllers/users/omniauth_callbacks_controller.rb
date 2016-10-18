49e65a3e71d74f3428b2c40c9d2cc8b1f4005ecb
Andrey Novikov <anovikov@at-consulting.ru>
2016-10-18 21:51:57 +0300

    Реализовано назначение очков за вход через соцсети
    См. https://gitlab.at-consulting.ru/dfp/rc16/issues/7

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,83 +2,80 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: [:open_id]
 
   PROVIDER_ATTRIBUTES_TEMPLATE = {
-    full_name:     lambda { |auth| auth.info.name },
-    email:         lambda { |auth| auth.info.email },
-    nickname:      lambda { |auth| auth.info.nickname },
-    gender:        lambda { |auth| auth.extra.raw_info.gender },
-    date_of_birth: lambda { |auth| ((Date.parse(auth.extra.raw_info.birthday) rescue nil)  if auth.extra.raw_info.birthday.present?) },
+    full_name:     lambda { |auth, _user| auth.info.name },
+    email:         lambda { |auth, _user| auth.info.email },
+    nickname:      lambda { |auth, _user| auth.info.nickname },
+    gender:        lambda { |auth, _user| auth.extra.raw_info.gender },
+    date_of_birth: lambda { |auth, _user| ((Date.parse(auth.extra.raw_info.birthday) rescue nil)  if auth.extra.raw_info.birthday.present?) },
+    points:        lambda do |auth, user|
+      will_be_rewarded = user.attributes["#{auth.provider}_uid"].blank? || user.new_record?
+      reward = will_be_rewarded ? Settings.sign_in_reward[auth.provider] : 0
+      user.points + reward
+    end
   }
 
   PROVIDERS = {
     facebook: PROVIDER_ATTRIBUTES_TEMPLATE.merge(
-      date_of_birth: lambda { |auth| ((Date.strptime(auth.extra.raw_info.birthday, "%m/%d/%Y") rescue nil)  if auth.extra.raw_info.birthday.present?) },
-      facebook_url:  lambda { |auth| auth.info.urls&.dig('Facebook') },
-      website_url:   lambda { |auth| auth.info.urls&.dig('Website') },
+      date_of_birth: lambda { |auth, _user| ((Date.strptime(auth.extra.raw_info.birthday, "%m/%d/%Y") rescue nil)  if auth.extra.raw_info.birthday.present?) },
+      facebook_url:  lambda { |auth, _user| auth.info.urls&.dig('Facebook') },
+      website_url:   lambda { |auth, _user| auth.info.urls&.dig('Website') },
     ),
     vkontakte: PROVIDER_ATTRIBUTES_TEMPLATE.merge(
-      date_of_birth: lambda { |auth| ((Date.parse(auth.extra.raw_info.bdate) rescue nil)  if auth.extra.raw_info.bdate.present?) },
-      gender:        lambda { |auth| {2 => 'male', 1 => 'female'}[auth.extra.raw_info.sex] },
-      vkontakte_url: lambda { |auth| auth.info.urls['Vkontakte'] },
+      date_of_birth: lambda { |auth, _user| ((Date.parse(auth.extra.raw_info.bdate) rescue nil)  if auth.extra.raw_info.bdate.present?) },
+      gender:        lambda { |auth, _user| {2 => 'male', 1 => 'female'}[auth.extra.raw_info.sex] },
+      vkontakte_url: lambda { |auth, _user| auth.info.urls['Vkontakte'] },
     ),
     github: PROVIDER_ATTRIBUTES_TEMPLATE.merge(
-      github_url:   lambda { |auth| auth.extra.raw_info.html_url },
-      website_url:  lambda { |auth| auth.extra.raw_info.blog },
-      hireable:     lambda { |auth| auth.extra.raw_info.hireable },
+      github_url:   lambda { |auth, _user| auth.extra.raw_info.html_url },
+      website_url:  lambda { |auth, _user| auth.extra.raw_info.blog },
+      hireable:     lambda { |auth, _user| auth.extra.raw_info.hireable },
     ),
   }
 
   # Create callback methods dynamically for each provider
   PROVIDERS.each do |provider, attrs_stub|
     define_method(provider) do
-      auth = request.env['omniauth.auth']
-      logger.debug auth.to_yaml
-      attributes = Hash[attrs_stub.map { |k, v| [k, v.call(auth)] }]
+      User.transaction do
+        auth = request.env['omniauth.auth']
+        logger.debug auth.to_yaml
+        @user = user_signed_in? ? current_user.lock! : User.lock.where("#{provider}_uid" => auth.uid).first_or_initialize
 
-      if user_signed_in? # Authenticated user connects to provider from profile page
-        User.transaction do
-          current_user.update("#{provider}_uid" => auth.uid)
-          update_user_info(current_user, attributes)
-        end
-        redirect_to profile_users_path
+        attributes = Hash[attrs_stub.map { |k, v| [k, v.call(auth, @user)] }]
 
-      else # User tries to sign in with provider
-        @user = User.where("#{provider}_uid" => auth.uid).first_or_initialize
-
-        if @user.persisted? # Try to update user info if not filled
+        if user_signed_in? # Authenticated user connects to provider from profile page
+          @user["#{provider}_uid"] = auth.uid
           update_user_info(@user, attributes)
-        elsif User.where(email: attributes[:email]).any?
-          @user = User.find_by(email: attributes[:email])
-          @user.public_send("#{provider}_uid=", auth.uid)
-          update_user_info(@user, attributes)
-        else
-          @user.update(
-            attributes.merge(
-              password: Devise.friendly_token[0, 20],
-              confirmed_at: Time.now,
+          redirect_to profile_users_path
+
+        else # User tries to sign in with provider
+          if @user.persisted? # Try to update user info if not filled
+            update_user_info(@user, attributes)
+          else
+            @user.update(
+              attributes.merge(
+                password: Devise.friendly_token[0, 20],
+                confirmed_at: Time.now,
+              )
             )
-          )
-        end
+          end
 
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: I18n.t(provider, scope: 'application.user.signed_via')) if is_navigational_format?
-        else
-          session['devise.user_attributes'] = @user.attributes.merge('confirmed_at' => nil)
-          redirect_to new_user_registration_url, alert: I18n.t('application.user.please_specify_email')
+          if @user.persisted?
+            sign_in_and_redirect @user, event: :authentication
+            set_flash_message(:notice, :success, kind: I18n.t(provider, scope: 'application.user.signed_via')) if is_navigational_format?
+          else
+            session['devise.user_attributes'] = @user.attributes.merge('confirmed_at' => nil)
+            redirect_to new_user_registration_url, alert: I18n.t('application.user.please_specify_email')
+          end
         end
       end
     end
-
   end
 
   # Try to update user info if not filled
   def update_user_info(user, attributes)
-    attributes.each do |attr, value|
-      if user.attributes[attr.to_s].blank? && value.present?
-        user.send("#{attr}=", value)
-      end
-    end
-    user.save if user.changed?
+    user.update!(attributes.select do |attr, value|
+      attr.in?(%i[points]) || (user.attributes[attr.to_s].blank? && value.present?)
+    end)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004170701) do
+ActiveRecord::Schema.define(version: 20161018064445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20161004170701) do
     t.string   "vkontakte_url",                                    comment: "Адрес профиля пользователя в VK"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "points",                 default: 0,  null: false, comment: "Сумма набранных очков за пройденные квесты"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["facebook_uid"], name: "index_users_on_facebook_uid", unique: true, using: :btree
