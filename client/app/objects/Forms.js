2816bcb4451c5594675a6bf979e4ecc5bc07a280
Kostas <kkryarov@at-consulting.ru>
2016-10-19 20:44:24 +0300

    RC-16 Верстка анкеты.
    * Когда открывается меню или модальное окно делаю body fixed, чтобы не скроллился контент

--- client/app/objects/Forms.js
+++ client/app/objects/Forms.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Jquery UJS ajax events
+ */
 $(document)
   .on('ajax:error', function(event, xhr, status, error) {
     /** Вставляем ошибку в ноду **/
@@ -8,4 +11,19 @@ $(document)
   .on('ajax:success', function(data, status, xhr) {
     /** Перезагружаем страницу **/
     Turbolinks.visit(location.toString());
+  });
+
+/**
+ * Показываем/скрываем галочку в инпуте в зависимости от того, введены ли данные
+ */
+$(document)
+  .on('focusout', '.js-form input', function() {
+    const $input = $(this);
+    const $wrapper = $input.parent();
+
+    if ($input.val().trim()) {
+      $wrapper.addClass('input-wrapper--icon-visible')
+    } else {
+      $wrapper.removeClass('input-wrapper--icon-visible')
+    }
   });
--- client/app/objects/Scroll.js
+++ client/app/objects/Scroll.js
@@ -9,24 +9,12 @@ $(document).on('click', '.js-scroll-rules', () => {
   }, 500);
 });
 
-const preventScroll = function(e) {
-  e.preventDefault();
-};
-
 /** Убираем скролл **/
-$('body').on('remove:scroll', function() {
-  const $body = $(this);
-
-  $body
-    .on('touchmove', preventScroll)
-    .addClass('body-no-scroll');
+$(document).on('remove:scroll', function() {
+  $('body').addClass('body-no-scroll');
 });
 
 /** Вовращаем скролл **/
-$('body').on('add:scroll', function() {
-  const $body = $(this);
-
-  $body
-    .off('touchmove', preventScroll)
-    .removeClass('body-no-scroll');
+$(document).on('add:scroll', function() {
+  $('body').removeClass('body-no-scroll');
 });
